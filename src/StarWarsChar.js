a6b22bb60eaedd61732550b6d81d01f41fdd7c29
Elliott Cheifetz <elliottsmyname@gmail.com>
2022-05-10 17:11:49 -0700

    created files and html for app.js

---
+++ src/StarWarsChar.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function StarWarsChar() {
+  return (
+    <div>StarWarsChar</div>
+  );
+}
---
+++ src/StarWarsList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function StarWarsList() {
+  return (
+    <div>
+      {/* mapping stuff */}
+    </div>
+  );
+}
